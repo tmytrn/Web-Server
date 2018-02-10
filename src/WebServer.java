ed7fbd4a1624a377a90957e71c65e6616b17a97f
niszeto <niszeto@gmail.com>
2018-02-10 11:23:00 -0800

    worker class can create requests and resources and cleaned up webserver class

--- src/WebServer.java
+++ src/WebServer.java
@@ -4,65 +4,50 @@ import java.util.*;
 
 public class WebServer {
 
-  public static final int DEFAULTPORT = 8096;
+  private static final String HTTPDCONFPATH = "conf/httpd.conf";
+  private static final String MIMETYPESPATH = "conf/mime.types";
 
-  public static void main( String[] args ) {
-    MimeTypes mimesFile = new MimeTypes( "conf/mime.types" );
-    mimesFile.load();
-    HttpdConf configuration = new HttpdConf( "conf/httpd.conf" );
-    configuration.load();
+  private ServerSocket socket;
+  private HttpdConf configuration;
+  private MimeTypes mimeTypes;
 
-//    System.out.println(configuration.lookup("/ab/"));
-    System.out.println(configuration.lookup("/"));
+  public WebServer( ) {
+    this.configuration = new HttpdConf( HTTPDCONFPATH );
+    this.mimeTypes = new MimeTypes( MIMETYPESPATH );
+    this.loadConfigurationFiles();
+    this.listenToPort();
+  }
 
-    //    //    configuration.load();
-    //    //    System.out.println(configuration.lookup("Listen"));
-    //    //    //System.out.println( mimesFile.lookup( "mp3" ) );
+  public void loadConfigurationFiles( ) {
+    this.configuration.load();
+    this.mimeTypes.load();
+  }
 
-    Request getRequest = new Request( "GET / HTTP/1.1\n" +
-        "Transfer-Encoding: chunked\n" +
-        "Date: Sat, 28 Nov 2009 04:36:25 GMT\n" +
-        "Server: LiteSpeed\n" +
-        "Connection: close\n" +
-        "X-Powered-By: W3 Total Cache/0.8\n" +
-        "Pragma: public\n" +
-        "Expires: Sat, 28 Nov 2009 05:36:25 GMT\n" +
-        "Etag: \"pub1259380237;gz\"\n" +
-        "Cache-Control: max-age=3600, public\n" +
-        "Content-Type: text/html; charset=UTF-8\n" +
-        "Last-Modified: Sat, 28 Nov 2009 03:50:37 GMT\n" +
-        "X-Pingback: http://net.tutsplus.com/xmlrpc.php\n" +
-        "Content-Encoding: gzip\n" +
-        "Vary: Accept-Encoding, Cookie, User-Agent\n" );
-
-    Resource resource = new Resource( "/", configuration );
-
-//    System.out.print( "methods\n" );
-//    System.out.print( "headers\n" );
-//    System.out.print("\r\n" );
-//    System.out.print( "body\n" );
-//    System.out.print("\r\n" );
-
+  public void listenToPort( ) {
     Socket client = null;
     int numberOfRequests = 0;
+    int portNumber = Integer.parseInt( this.configuration.lookup( "Listen" ) );
 
     try {
-      ServerSocket serverSocket = new ServerSocket( DEFAULTPORT );
+      this.socket = new ServerSocket( portNumber );
 
-      while(true){
-        client = serverSocket.accept();
-        Request request = new Request( client.getInputStream() );
-        System.out.println( ++numberOfRequests );
-        client.close();
+      while ( true ) {
+        client = this.socket.accept();
+        System.out.println( "Request Number is: " + ++numberOfRequests );
+
+        Worker worker = new Worker( client, this.configuration, this.mimeTypes );
+
+        Thread thread = new Thread( worker, Integer.toString( numberOfRequests ) );
+        thread.start();
+
       }
 
+    } catch ( Exception e ) {
+      e.printStackTrace();
     }
-    catch (Exception e){
-      //File document = new File(configuration.lookup("DirectoryIndex"));
-      //e.printStackTrace();
-      //404 error
-    }
+  }
 
-
+  public static void main( String[] args ) {
+    WebServer webServer = new WebServer();
   }
 }
---
+++ src/Worker.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.net.*;
+
+public class Worker implements Runnable {
+  private Socket client;
+  private MimeTypes mimes;
+  private HttpdConf config;
+
+  public Worker( Socket client, HttpdConf config, MimeTypes mimes ) {
+    this.client = client;
+    this.mimes = mimes;
+    this.config = config;
+  }
+
+  public void run( ) {
+    Request request;
+    Resource resource;
+
+    try {
+      request = new Request( this.client.getInputStream() );
+      resource = new Resource( request.getUri(), this.config );
+
+//      System.out.println( "resource and request made" );
+
+      //create response
+      //send response back to stream
+
+      this.client.close();
+    } catch ( IOException e ) {
+      e.printStackTrace();
+    }
+  }
+}
